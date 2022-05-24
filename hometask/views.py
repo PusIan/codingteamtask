5b54af1e5998820d8131a4e4e4106746ce189713
Ruslan Gilmanov <studentoil@yandex.ru>
2022-05-25 03:24:42 +0500

    add strange filtering, orm is hard, select is easier

--- hometask/views.py
+++ hometask/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 from .serializers import FoodListSerializer
-from .models import FoodCategory
+from .models import FoodCategory, Food
+from django.db.models import Prefetch, Case, When
 
 
 # Create your views here.
 class FoodsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = FoodCategory.objects.all()
+    queryset = FoodCategory.objects.prefetch_related(
+        Prefetch(
+            'food', queryset=Food.objects.filter(is_publish=True))).annotate(
+                food_cnt=Case(When(food__is_publish=True, then=1))).filter(
+                    food_cnt__gt=0).distinct()
     serializer_class = FoodListSerializer
